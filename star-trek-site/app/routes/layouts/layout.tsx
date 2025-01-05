d46ac47755433bbcc0a64dea6601d948bdd0df78
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2025-01-04 23:02:56 -0800

    Added layout and updated routes

---
+++ star-trek-site/app/routes/layouts/layout.tsx
@@ -0,0 +1,16 @@
+import { Link, Outlet } from 'react-router'
+
+export default function Layout() {
+    return (
+        <div className='flex flex-col bg-slate-800 text-white min-h-svh'>
+            <nav className='flex justify-center py-4 border-b-2 border-white'>
+                <div className="text-xl">
+                    <Link to="/">Home</Link>
+                </div>
+            </nav>
+            <main className='flex-1 p-4 flex justify-center items-center'>
+                <Outlet />
+            </main>
+        </div>
+    )
+}
