32024607e17ceb01aad3e362ec4f40eec38c7513
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2025-01-06 15:01:26 -0800

    Changed height from svh to dvh in layouts

--- star-trek-site/app/routes/layouts/homeLayout.tsx
+++ star-trek-site/app/routes/layouts/homeLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router'
 
 export default function HomeLayout() {
     return (
-        <div className='bg-stars bg-cover bg-center flex flex-col bg-slate-800 text-white min-h-svh'>
+        <div className='bg-stars bg-cover bg-center flex flex-col bg-slate-800 text-white min-h-dvh'>
             <main className='flex-1 p-4 flex justify-center items-center'>
                 <Outlet />
             </main>
