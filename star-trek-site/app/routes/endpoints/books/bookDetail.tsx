85f54ebea53f324b38a8bbe9337d81b8288b35d7
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2025-01-07 22:09:25 -0800

    Added more info to book detail pages

--- star-trek-site/app/routes/endpoints/books/bookDetail.tsx
+++ star-trek-site/app/routes/endpoints/books/bookDetail.tsx
@@ -16,25 +16,48 @@ export default function BookDetail() {
         <main className="flex flex-col justify-center items-center text-center
          gap-y-4">
             <h1 className="text-3xl font-bold">Details about {data.book.title}:</h1>
-            <div className="flex flex-col justify-center items-center gap-y-4 text-2xl text-center">
+            <div className="flex flex-col justify-center items-center gap-y-4 text-xl text-center">
                 {data.book.publishedMonth && data.book.publishedDay && data.book.publishedYear && (
                     <h2>
-                        Publish date: {data.book.publishedMonth}/{data.book.publishedDay}/{data.book.publishedYear}
+                        <span className="text-2xl">Publish date: </span>{data.book.publishedMonth}/{data.book.publishedDay}/{data.book.publishedYear}
                     </h2>
                 )}
                 {data.book.anthology !== undefined && data.book.anthology !== null &&
-                    <h2>Anthology: {data.book.anthology ? "Yes" : "No"}</h2>
+                    <h2><span className="text-2xl">Anthology: </span>{data.book.anthology ? "Yes" : "No"}</h2>
                 }
                 {data.book.audiobook !== undefined && data.book.audiobook !== null &&
-                    <h2>Audiobook available: {data.book.audiobook ? "Yes" : "No"}</h2>
+                    <h2><span className="text-2xl">Audiobook available: </span>{data.book.audiobook ? "Yes" : "No"}</h2>
                 }
                 {data.book.authors.length > 0 && (
-                    <div className="flex flex-col md:flex-row items-center gap-x-2">
-                        <h2>Authors: </h2>
+                    <div className="flex flex-col md:flex-row items-center md:gap-x-2 gap-y-2">
+                        <h2 className="text-2xl">Authors: </h2>
                         {data.book.authors && data.book.authors?.map((author: any, index: number) => (
                             <h2 key={author.uid}>
                                 {author.name}
                                 {index < data.book.authors.length - 1 && ","}</h2>
+                        ))}
+                    </div>
+                )}
+                {data.book.characters.length > 0 && (
+                    <div className="flex flex-col lg:flex-row lg:w-96 justify-center items-center gap-2 flex-wrap">
+                        <h2 className="text-2xl">Characters: </h2>
+                        {data.book.characters && data.book.characters?.map((characters: any, index: number) => (
+                            <h2 key={characters.uid}>
+                                {characters.name}
+                                {index < data.book.characters.length - 1 && ","}</h2>
+                        ))}
+                    </div>
+                )}
+                {data.book.numberOfPages !== undefined && data.book.numberOfPages !== null &&
+                    <h2><span className="text-2xl">Number of pages: </span>{data.book.numberOfPages}</h2>
+                }
+                {data.book.publishers.length > 0 && (
+                    <div className="flex flex-col items-center gap-2">
+                        <h2 className="text-2xl">Publishers: </h2>
+                        {data.book.publishers && data.book.publishers?.map((publishers: any, index: number) => (
+                            <h2 key={publishers.uid}>
+                                {publishers.name}
+                                {index < data.book.publishers.length - 1 && ","}</h2>
                         ))}
                     </div>
                 )}
