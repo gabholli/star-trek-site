d037b771f19c198952214dadb894a4cc3f927eb3
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2025-01-31 19:34:12 -0800

    Updated home page

--- star-trek-site/app/routes/home.tsx
+++ star-trek-site/app/routes/home.tsx
@@ -3,16 +3,19 @@ import type { Route } from "./+types/home"
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center gap-y-16">
-      <h1 className="text-3xl text-center font-bold">Welcome to Star Trek <span className="text-blue-400">Explorer!</span></h1>
-      <nav className="text-2xl grid grid-cols-1 place-items-center gap-y-16">
-        <Link
-          className="lg:hover:underline"
-          to="endpoints"
-        >
-          List of categories
-        </Link>
-      </nav>
+    <div className="flex flex-col lg:flex-row justify-center items-center gap-y-12 lg:gap-x-12">
+      <div className="flex flex-col gap-y-8">
+        <h1 className="text-3xl text-center font-bold">Welcome to Star Trek <span className="text-blue-400">Explorer!</span></h1>
+        <nav className="text-2xl grid grid-cols-1 place-items-center gap-y-16">
+          <Link
+            className="lg:hover:underline border-black border-2 rounded-full px-4 py-2"
+            to="endpoints"
+          >
+            List of categories
+          </Link>
+        </nav>
+      </div>
+      <img className="lg:size-1/2" src=" /stars3.png" />
     </div>
   )
 }
