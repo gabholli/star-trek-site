35563dcbeda46a2ff899c0610e2246aecfc7553d
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2025-02-01 16:06:27 -0800

    Updated home page

--- star-trek-site/app/routes/layouts/homeLayout.tsx
+++ star-trek-site/app/routes/layouts/homeLayout.tsx
@@ -1,8 +1,18 @@
-import { Outlet } from 'react-router'
+import { Link, Outlet } from 'react-router'
 
 export default function HomeLayout() {
     return (
         <div className='font-lato bg-cover bg-center flex flex-col min-h-dvh'>
+            <nav className='flex justify-center py-4 border-b-2 border-black'>
+                <div className="flex text-xl gap-x-8">
+                    <Link
+                        to="/credits"
+                        className='lg:hover:underline'
+                    >
+                        Credits
+                    </Link>
+                </div>
+            </nav>
             <main className='flex-1 p-4 flex justify-center items-center'>
                 <Outlet />
             </main>
