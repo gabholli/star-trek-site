b030eec7ae5ef4bfddec314cf7e1d2671403b5a6
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2025-03-02 16:43:13 -0800

    Update endpoints.tsx

--- star-trek-site/app/routes/endpoints/endpoints.tsx
+++ star-trek-site/app/routes/endpoints/endpoints.tsx
@@ -2,25 +2,25 @@ import { Link } from "react-router";
 
 export default function Endpoints() {
   return (
-    <div className="flex flex-col justify-center items-center gap-y-4">
+    <div className="flex flex-col justify-center items-center gap-y-10">
       <div className="text-3xl font-bold text-blue-400">
         <h1>Select a category:</h1>
       </div>
-      <main className="text-2xl flex flex-col justify-center items-center gap-y-4">
+      <main className="text-2xl flex flex-col justify-center items-center gap-y-10">
         <Link
-          className=" lg:hover:underline"
+          className=" lg:hover:underline border-black rounded-full border-2 py-2 px-4"
           to="/books"
         >
           Books
         </Link>
         <Link
-          className=" lg:hover:underline"
+          className=" lg:hover:underline border-black rounded-full border-2 py-2 px-4"
           to="/episodes"
         >
           Episodes
         </Link>
         <Link
-          className=" lg:hover:underline"
+          className=" lg:hover:underline border-black rounded-full border-2 py-2 px-4"
           to="/movies"
         >
           Movies
